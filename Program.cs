9f167c745c52eb6bad58decf00ddef96680e43e9
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-08-02 11:45:57 +0300

    Added web app

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Everyone!");
         }
     }
 }
