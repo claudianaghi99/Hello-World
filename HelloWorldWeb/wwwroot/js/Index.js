3bff597ccf3cee69a6e2b12b90ebb620623af971
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-08-03 10:35:17 +0300

    Index for Create button created

---
+++ HelloWorldWeb/wwwroot/js/Index.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+
+    $("#createButton").click(function () {
+        var newcomerName = $("#nameField").val();
+
+        // Remember string interpolation
+        $("#teamList").append(`<li>${newcomerName}</li>`);
+
+        $("#nameField").val("");
+    })
+});
