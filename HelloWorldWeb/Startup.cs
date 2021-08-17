7ffcd5772729755ccb76ef87de583a43b53e5a41
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-08-17 10:41:23 +0300

    Connectionstring added, starup changes

--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -29,6 +29,11 @@ namespace HelloWorldWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseNpgsql(
+            Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+           
             services.AddControllersWithViews();
             services.AddSingleton<IWeatherControllerSettings, WeatherControllerSettings>();
             services.AddSingleton<ITeamService>(new TeamService());
