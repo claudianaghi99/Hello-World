90f3e8a0fb4958d63fe14fa06f2500987b3930f2
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-08-19 09:46:21 +0300

    Json serialization in new project

---
+++ CSharpFeatures_C/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CSharpFeatures_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeamMember teamMember = new TeamMember() {Name="Member1"};
+            string jsonString = JsonSerializer.Serialize(teamMember);
+            Console.WriteLine(jsonString);
+            File.WriteAllText("TeamMember.json", jsonString);
+            var teamMemberDesearialized = JsonSerializer.Deserialize<TeamMember>(jsonString);
+        }
+    }
+}
---
+++ CSharpFeatures_C/TeamMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpFeatures_C
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthdate { get; set; }
+    }
+}
