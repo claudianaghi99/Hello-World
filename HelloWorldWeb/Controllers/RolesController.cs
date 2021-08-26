c598e6792a826d9260219f75dfa096fca46ddbe8
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-08-26 11:22:07 +0300

    Role create page added for role Administrator

--- HelloWorldWeb/Controllers/RolesController.cs
+++ HelloWorldWeb/Controllers/RolesController.cs
@@ -12,6 +12,7 @@
     {
         RoleManager<IdentityRole> roleManager;
 
+        // injection
         public RolesController(RoleManager<IdentityRole> roleManager)
         {
             this.roleManager = roleManager;
@@ -23,25 +24,26 @@
             return View(roleManager.Roles);
         }
 
-        // GET: RolesController/Details/5
+    /*    // GET: RolesController/Details/5
         public ActionResult Details(int id)
         {
             return View();
         }
+    */
 
         // GET: RolesController/Create
         public ActionResult Create()
         {
-            return View();
+            return View(new IdentityRole()); // object
         }
 
         // POST: RolesController/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(IdentityRole role)
         {
             try
             {
+                await roleManager.CreateAsync(role);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -50,46 +52,5 @@
             }
         }
 
-        // GET: RolesController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: RolesController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: RolesController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: RolesController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
