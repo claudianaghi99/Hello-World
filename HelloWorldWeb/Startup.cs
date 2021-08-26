fc06d322e520e30a141181447c0859ca222ccf8d
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-08-26 11:01:28 +0300

    Added Roles Controller for role management

--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -56,8 +56,12 @@ namespace HelloWorldWeb
 
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            /*   services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                  .AddEntityFrameworkStores<ApplicationDbContext>();
+            */
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+               .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddSingleton<IWeatherControllerSettings, WeatherControllerSettings>();
             services.AddScoped<ITeamService, DbTeamService>();
