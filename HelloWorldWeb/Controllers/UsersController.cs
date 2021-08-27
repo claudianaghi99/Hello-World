3103bea1ab3baa03f09b5f077a0a8a2cbe19824b
LAPTOP-FFQ4V0J6\Claudia <claudia.naghi@principal33.com>
2021-08-27 09:27:29 +0300

    Deleted comments, fixed warnings

--- HelloWorldWeb/Controllers/UsersController.cs
+++ HelloWorldWeb/Controllers/UsersController.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using HelloWorldWeb.Data;
-using HelloWorldWeb.Models;
-using Microsoft.AspNetCore.Identity;
-
-namespace HelloWorldWeb.Controllers
+﻿namespace HelloWorldWeb.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using HelloWorldWeb.Data;
+    using HelloWorldWeb.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.EntityFrameworkCore;
+
     public class UsersController : Controller
     {
-
         private UserManager<IdentityUser> userManager;
+
         public UsersController(UserManager<IdentityUser> userManager)
         {
             this.userManager = userManager;
@@ -39,132 +39,6 @@ namespace HelloWorldWeb.Controllers
             var user = await userManager.FindByIdAsync(id);
             await userManager.RemoveFromRoleAsync(user, "Administrators");
             return RedirectToAction(nameof(Index));
-
         }
-
-        //// GET: Users/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var user = await userManager.User
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(user);
-        //}
-
-        //// GET: Users/Create
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: Users/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,Name,Role")] User user)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        userManager.Add(user);
-        //        await userManager.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(user);
-        //}
-
-        //// GET: Users/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var user = await userManager.User.FindAsync(id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(user);
-        //}
-
-        //// POST: Users/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Role")] User user)
-        //{
-        //    if (id != user.Id)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            userManager.Update(user);
-        //            await userManager.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!UserExists(user.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(user);
-        //}
-
-        //// GET: Users/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var user = await userManager.User
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(user);
-        //}
-
-        //// POST: Users/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var user = await userManager.User.FindAsync(id);
-        //    userManager.User.Remove(user);
-        //    await userManager.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
-
-        //private bool UserExists(int id)
-        //{
-        //    return userManager.User.Any(e => e.Id == id);
-        //}
     }
 }
--- HelloWorldWeb/Startup.cs
+++ HelloWorldWeb/Startup.cs
@@ -3,6 +3,7 @@ namespace HelloWorldWeb
     using System;
     using System.IO;
     using System.Reflection;
+    using System.Threading.Tasks;
     using HelloWorldWeb.Controllers;
     using HelloWorldWeb.Data;
     using HelloWorldWeb.services;
@@ -40,6 +41,7 @@ namespace HelloWorldWeb
         public void ConfigureServices(IServiceCollection services)
         {
            string databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
             // string databaseUrl = Configuration.GetConnectionString("HerokuPostgres");
             string connectionString = "";
             if (databaseUrl != null)
@@ -76,7 +78,6 @@ namespace HelloWorldWeb
             services.AddSingleton<IBroadcastService, BroadcastService>();
 
             AssignRoleProgramatically(services.BuildServiceProvider());
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -116,7 +117,7 @@ namespace HelloWorldWeb
                 endpoints.MapHub<MessageHub>("/messagehub");
             });
 
-          //  AssignRoleProgramatically(app.ApplicationServices);
+         // AssignRoleProgramatically(app.ApplicationServices)
         }
 
         private async void AssignRoleProgramatically(IServiceProvider services)
@@ -124,6 +125,57 @@ namespace HelloWorldWeb
             UserManager<IdentityUser> userManager = services.GetRequiredService<UserManager<IdentityUser>>();
             IdentityUser user = await userManager.FindByNameAsync("claudianaghi99@gmail.com");
             await userManager.AddToRoleAsync(user, "Administrators");
+        }
+
+        private static async Task EnsureUsersCreated(IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            var adminUser = await EnsureUserCreated(userManager, "borys.lebeda@principal33.com", "TzfOh22_FCbjxXQt6U");
+            var operatorUser = await EnsureUserCreated(userManager, "borys.lebeda2@principal33.com", "TzfOh22_FCbjxXQt6U2");
+
+            var adminRole = await EnsureRoleCreated(serviceProvider, "Administrator");
+            var operatorRole = await EnsureRoleCreated(serviceProvider, "Operator");
+
+            await userManager.AddToRoleAsync(adminUser, adminRole.Name);
+            await userManager.AddToRoleAsync(operatorUser, operatorRole.Name);
+
+            var users = await userManager.Users.ToListAsync();
+            Console.WriteLine($"There are {users.Count} users now.");
+        }
+
+        private static async Task<IdentityUser> EnsureUserCreated(UserManager<IdentityUser> userManager, string name, string password)
+        {
+            var adminUser = await userManager.FindByNameAsync(name);
+            if (adminUser == null)
+            {
+                await userManager.CreateAsync(new IdentityUser(name));
+                adminUser = await userManager.FindByNameAsync(name);
+                var tokenChangePassword = await userManager.GeneratePasswordResetTokenAsync(adminUser);
+
+                var result = await userManager.ResetPasswordAsync(adminUser, tokenChangePassword, password);
+                if (!adminUser.EmailConfirmed)
+                {
+                    var token = await userManager.GenerateEmailConfirmationTokenAsync(adminUser);
+                    await userManager.ConfirmEmailAsync(adminUser, token);
+                }
+            }
+
+            return adminUser;
+        }
+
+        private static async Task<IdentityRole> EnsureRoleCreated(ServiceProvider serviceProvider, string roleName)
+        {
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+            var adminRole = await roleManager.FindByNameAsync(roleName);
+            if (adminRole == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+                adminRole = await roleManager.FindByNameAsync(roleName);
+            }
+
+            return adminRole;
         }
     }
 }
